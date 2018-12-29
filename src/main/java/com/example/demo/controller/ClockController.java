0e8fb5a8304efa3db336b29977ba794d1ede7821
policy <lifangzhen@juniuo.com>
2018-12-29 15:07:08 +0800

    ss

--- src/main/java/com/example/demo/controller/ClockController.java
+++ src/main/java/com/example/demo/controller/ClockController.java
@@ -1,18 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.ClockDao;
+import com.example.demo.model.Clock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by 26949 on 2018/12/29.
  */
 @Controller
 public class ClockController {
+    @Autowired private ClockDao clockDao;
     @RequestMapping("/clock.htm")
     public ModelAndView clock(){
+        List<Clock> list = clockDao.findAll();
+        Clock clock = new Clock();
+        clock.setId("2018-12-29");
+        clock.setStart1("8:00");
+        clock.setEnd1("9:00");
+        clock.setStart2("10:00");
+        clock.setEnd2("11:00");
+        clock.setStart3("13:00");
+        clock.setEnd3("18:00");
+        list.add(clock);
         ModelAndView mav = new ModelAndView("/clock");
+        mav.addObject("list", list);
         return mav;
     }
 
--- src/main/resources/templates/clock.ftl
+++ src/main/resources/templates/clock.ftl
@@ -9,5 +9,33 @@
 <button>打卡开始</button>
 <button>打卡结束</button>
 
+<table>
+    <tr>
+        <td>日  期</td>
+        <td>时段一</td>
+        <td>时段二</td>
+        <td>时段三</td>
+    </tr>
+    <#list list as clock>
+        <tr>
+            <td>${clock.id}</td>
+            <#if clock.start1??&&clock.end1??>
+                <td>${clock.start1}-${clock.end1}</td>
+            <#else>
+                <td>----</td>
+            </#if>
+            <#if clock.start2??&&clock.end2??>
+                <td>${clock.start2}-${clock.end2}</td>
+            <#else>
+                <td>----</td>
+            </#if>
+            <#if clock.start3??&&clock.end3??>
+                <td>${clock.start3}-${clock.end3}</td>
+            <#else>
+                <td>----</td>
+            </#if>
+        </tr>
+    </#list>
+</table>
 </body>
 </html>
